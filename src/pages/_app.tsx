c4125605d1f9afed3c42fb18f1a575064ce28c59
Vatsal Sakariya <vsakariya8@gmail.com>
2023-01-09 11:35:36 +0530

    added progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,33 @@ import { ToastProvider } from "../contexts";
 import { ToastContainer } from "../components";
 import { trpc } from "../utils";
 import { Footer } from "../components";
+import { useRouter } from "next/router";
+import { start, done, configure } from "nprogress";
+import { useEffect } from "react";
 import { type Session } from "next-auth";
 import { type AppType } from "next/app";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+  configure({ showSpinner: false });
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => start());
+    router.events.on("routeChangeComplete", () => done());
+    router.events.on("routeChangeError", () => done());
+
+    return () => {
+      router.events.off("routeChangeStart", () => start());
+      router.events.off("routeChangeComplete", () => done());
+      router.events.off("routeChangeError", () => done());
+    };
+  }, [router.events]);
+
   return (
     <>
       <SessionProvider session={session}>
